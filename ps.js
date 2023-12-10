e2b5a53d8e67c06cafb3cfd7fba48a7c942df981
Veshal Raj <veshalraj1307@gmail.com>
2023-12-10 19:22:19 +0530

    using ES6 syntax

--- ps.js
+++ ps.js
@@ -4,14 +4,21 @@
  *      Amazon interview question  
  */
 
-let sum = function(a) {
-    return function(b) {
-        if (b) {
-            return sum(a+b)
-        }
-        return a;
+// let sum = function(a) {
+//     return function(b) {
+//         if (b) {
+//             return sum(a+b)
+//         }
+//         return a;
+//     }
+// }
+
+let sum =(a) => {
+    return (b)=> {
+    return b?sum(a+b):a
     }
 }
+
 
 let result = sum(1)(2)(3)(4)(5)(6)(7)(8)(9)(10)()
 console.log(result)
