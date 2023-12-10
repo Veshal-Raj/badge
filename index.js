6ee4e371083737a7f9950fc3ddc408c6c9e345cf
Veshal Raj <veshalraj1307@gmail.com>
2023-12-10 15:41:36 +0530

    currying using bind method more examples

--- index.js
+++ index.js
@@ -2,5 +2,31 @@ let multiply = function(x,y) {
   console.log(x*y)
 }
 
-let multiply2 = multiply.bind(this,2)
+// let multiply2 = multiply.bind(this,2)
+// multiply2(5)
+
+/**
+ * 
+ *  here what bind method is doing is that,
+ *  it will create a copy fo the multiply function 
+ *  and we are doing operaton on the copy of the 
+ *  multiply function.
+ * 
+ */
+
+let multiply2 = multiply.bind(this,2,3)
 multiply2(5)
+
+/**
+ *     output is 6, because here the x will be taken as 2 and
+ *     the y is taken as 3, it will ignore the parameter of the multiply2().
+ *  
+ */
+
+let multiply3 = multiply.bind(this,2,3,3,4,56,4)
+multiply2(5)
+
+/**
+ *      output is 6, because the x will taken as 2 and 
+ *      the y wil be taken as 3, it will ignore all other paramaters unless we are using them.
+ */
