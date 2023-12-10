9d3cb2b303a9bb7b1ee4a35eb1fffa785fd279c8
Veshal Raj <veshalraj1307@gmail.com>
2023-12-10 15:59:33 +0530

    currying using closure

--- index.js
+++ index.js
@@ -1,38 +1,24 @@
-let multiply = function(x,y) {
-  console.log(x*y)
+// let multiply = function(x,y) {
+//   console.log(x*y)
+// }
+
+
+
+let multiply = function (x) {
+  return function(y) {
+    console.log(x*y)
+  }
 }
 
-// let multiply2 = multiply.bind(this,2)
-// multiply2(5)
+
+let multiply1 = multiply(2)
+multiply1(3)
+
+
+let multiply2 = multiply(2)
+multiply2(3)
+
 
 /**
- * 
- *  here what bind method is doing is that,
- *  it will create a copy fo the multiply function 
- *  and we are doing operaton on the copy of the 
- *  multiply function.
- * 
- */
-
-let multiply1 = multiply.bind(this)
-multiply1(5,2)
-
-// output is 10
-
-
-let multiply2 = multiply.bind(this,2,3)
-multiply2(5)
-
-/**
- *     output is 6, because here the x will be taken as 2 and
- *     the y is taken as 3, it will ignore the parameter of the multiply2().
- *  
- */
-
-let multiply3 = multiply.bind(this,2,3,3,4,56,4)
-multiply3(5)
-
-/**
- *      output is 6, because the x will taken as 2 and 
- *      the y wil be taken as 3, it will ignore all other paramaters unless we are using them.
+ *    This is using closure.
  */
