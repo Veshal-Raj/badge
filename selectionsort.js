63b0fb84affa1574c8c63518d964afb3067928db
Veshal Raj <veshalraj1307@gmail.com>
2023-12-14 08:53:55 +0530

    selection sort

---
+++ selectionsort.js
@@ -0,0 +1,22 @@
+function selectionSort(arr) {
+    const n = arr.length;
+
+    for (let i = 0; i < n - 1; i++) {
+        let minIndex = i;
+
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // Swap arr[i] and arr[minIndex]
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+
+    return arr;
+}
+
+// Example usage:
+const arr2 = [64, 25, 12, 22, 11];
+console.log("Selection Sort:", selectionSort(arr2.slice()));
