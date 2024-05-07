05c1477a2b939f32c9f60f5e252d9f5ddf77215f
Veshal Raj <veshalraj1307@gmail.com>
2024-05-07 23:55:39 +0530

    update: added more questions

--- js/object.js
+++ js/object.js
@@ -165,3 +165,175 @@
  * 
  */
 
+//-------------------------------------------------------//
+
+// what is the output?
+
+// console.log([..."Hello"]) // ['H', 'e', 'l', 'l', 'o']
+
+//-----------------------------------------------------//
+
+/**
+ *  const user = { name: 'veshal', age: 22 }
+    const user2 = { admin: true, ...user }
+
+    console.log(user2) // { admin: true, name: 'veshal', age: 22 } 
+ * 
+ */
+
+//-------------------------------------------------------//
+// what is the output
+
+/**
+ *      const settings = {
+            username: "Piyush",
+            level: 19,
+            health: 90
+        }
+
+        const data = JSON.stringify(settings, ["level", "health"]) // (this will only stringify level and health)
+        console.log(data) // {"level":19,"health":90}
+ * 
+ */
+
+//--------------------------------------------------------------------//
+
+// what is the output
+
+/**
+ * 
+      const shape = {
+            radius: 10,
+            diameter() {
+                return this.radius * 2;
+            },
+            perimeter: () => 2 * Math.PI * this.radius
+        }
+
+        console.log(shape.diameter()) // 20
+        console.log(shape.perimeter()) // NaN
+ * 
+ */
+
+//------------------------------------------------------------------------//
+// what is the output
+
+/**     
+ *      let user = {
+            name: 'Piyush',
+            age: 56,
+            fullName: {
+                first: 'hello',
+                second: 'man'
+            }
+        }
+
+        const {fullName: { first }} = user
+
+        console.log(first)
+ */
+
+//-------------------------------------------------------------------------//
+
+// what is the output
+
+/**
+ *      function getItems(fruitList,  favouriteFruit, ...args) {
+            return [...fruitList, ...args, favouriteFruit]
+        }
+
+        console.log(getItems(["banana", "apple"], "pear", "orange"))  // [ 'banana', 'apple', 'orange', 'pear' ]
+ * 
+ */
+
+//--------------------------------------------------------------------------//
+// what is the output
+
+/**
+ *      let c = { greetings: "hey!" }
+        let d 
+        d = c
+
+        c.greetings = "hello"
+        console.log(d.greetings) // hello
+ * 
+ */
+
+//------------------------------------------------------------------------//
+
+// what is the output
+
+// console.log({a: 1} == { a: 1}) // false
+// console.log({a: 1} === { a: 1}) // false
+
+//------------------------------------------------------------------------//
+
+// what is the output
+
+/** * 
+ *  let person = {name: 'hi'};
+    const members = [person]
+    person = null
+
+    console.log(members)
+ * 
+ */
+
+//-----------------------------------------------------------------------//
+
+// what is the output
+
+/**
+ *  const value = { number: 10 }
+
+    const multiply = (x={...value}) => {
+        console.log((x.number *= 2))
+    }
+
+    multiply() // 20
+    multiply() // 20
+    multiply(value) // 20
+    multiply(value) // 40
+ * 
+ */
+
+//----------------------------------------------------------------------------//
+
+// what is the output
+
+/**
+ *      
+function changeAgeAndReference(person) {
+    person.age = 25;
+    person ={
+        name: 'John',
+        age: 50
+    }
+    return person
+}
+
+const personObj1 = {
+    name: 'Alex',
+    age: 55
+}
+
+const person2 = changeAgeAndReference(personObj1)
+
+console.log(personObj1) // { name: 'Alex', age: 25 }
+console.log(person2) // { name: 'John', age: 50 }
+ * 
+ */
+
+//-----------------------------------------------------------------------//
+
+// What is shallow copy and what is deep copy of an object? 
+// how to deep copy / clone an object?
+
+let user = {
+    name: 'Roadside coder',
+    age: 24
+}
+
+// const objClone = Object.assign({}, user)
+// const objClone = JSON.parse(JSON.stringify(user))
+const objClone = {...user}
